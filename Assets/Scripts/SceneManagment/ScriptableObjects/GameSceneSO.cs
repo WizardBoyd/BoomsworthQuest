399c11b5e8d0e76ea0f6d2b65cd76f9e87d53e86
WizardBoyd <luisboyd2000@gmail.com>
2024-06-23 21:24:36 +0100

    changes

--- Assets/Scripts/SceneManagment/ScriptableObjects/GameSceneSO.cs
+++ Assets/Scripts/SceneManagment/ScriptableObjects/GameSceneSO.cs
@@ -1,18 +1,24 @@
 
 
+using System.Collections.Generic;
 using Audio.AudioData;
 using BaseClasses;
+using SceneManagment.ScriptableObjects.Actions;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 namespace SceneManagment.ScriptableObjects
 {
-    public class GameSceneSO : DescriptionBaseSO
+    [CreateAssetMenu(menuName = "Scene/Game Scene")]
+    public class GameSceneSO : ScriptableObject
     {
-
         public GameSceneType SceneType;
         public AssetReference SceneReference;
-        public AudioCueSO musicTrack;
-        
+
+        [Header("Actions To Perform On Entering")] [SerializeField]
+        public List<BaseSceneChangeAction> OnEnterSceneActions = new List<BaseSceneChangeAction>();
+        [Header("Actions to Perform On Leaving")]
+        [SerializeField]  public List<BaseSceneChangeAction> OnLeaveSceneActions = new List<BaseSceneChangeAction>();
         
         public enum GameSceneType
         {
@@ -20,11 +26,10 @@ namespace SceneManagment.ScriptableObjects
             Initialisation,
             PersistentManagers,
             Gameplay,
-            MainMenu,
             
             //Status Scenes
-            FailedLevelScene,
-            WinLevelScene,
+            Level,
+            Menu
             
         }
     }
